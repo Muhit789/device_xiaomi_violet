cbac3f434f58064fef2a918d552832ca58748faa
Muhit789 <165560731+Muhit789@users.noreply.github.com>
2024-10-24 20:10:19 +0600

    Add files via upload

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+git clone --depth=1 https://github.com/kibria5/android_vendor_xiaomi_violet.git -b udc vendor/xiaomi/violet
+git clone --depth=1 https://github.com/selfmusing/kernel_xiaomi_violet.git -b main kernel/xiaomi/violet
+git clone --depth=1 https://gitlab.com/CuriousNom/chaste_vendor-xiaomi-firmware-violet.git -b 14.0 vendor/xiaomi-firmware/violet
+git clone --depth=1 https://github.com/selfmusing/meme_cam.git -b 14 vendor/MiuiCamera
+git clone https://github.com/DerpFest-Devices/hardware_xiaomi.git -b 14 hardware/xiaomi
+
