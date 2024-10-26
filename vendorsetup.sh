7b5de2b75dabc9080fc27dd31db7de19e6692220
Mohiuddin Ahmed <165560731+Muhit789@users.noreply.github.com>
2024-10-26 13:50:15 +0600

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,7 @@
-git clone --depth=1 https://github.com/kibria5/android_vendor_xiaomi_violet.git -b udc vendor/xiaomi/violet
-git clone --depth=1 https://github.com/selfmusing/kernel_xiaomi_violet.git -b main kernel/xiaomi/violet
+git clone --depth=1 https://github.com/Muhit789/android_vendor_xiaomi_violet.git -b udc vendor/xiaomi/violet
+git clone --depth=1 https://github.com/Muhit789/kernel_xiaomi_violet.git -b main kernel/xiaomi/violet
 git clone --depth=1 https://gitlab.com/CuriousNom/chaste_vendor-xiaomi-firmware-violet.git -b 14.0 vendor/xiaomi-firmware/violet
 git clone --depth=1 https://github.com/selfmusing/meme_cam.git -b 14 vendor/MiuiCamera
 git clone https://github.com/DerpFest-Devices/hardware_xiaomi.git -b 14 hardware/xiaomi
 
+export USE_CCACHE=1 && ccache -M 60G && export CONFIG_STATE_NOTIFIER=y && export SELINUX_IGNORE_NEVERALLOWS=true
