8ca024d5301b3568ae3874b941567d1322de3e3d
Mohiuddin Ahmed <165560731+Muhit789@users.noreply.github.com>
2024-11-01 23:55:03 +0600

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
-git clone --depth=1 https://github.com/Muhit789/android_vendor_xiaomi_violet.git -b udc vendor/xiaomi/violet
+git clone --depth=1 https://github.com/Muhit789/vendor_xiaomi_violet.git -b udc vendor/xiaomi/violet
 git clone --depth=1 https://github.com/Muhit789/kernel_xiaomi_violet.git -b main kernel/xiaomi/violet
 git clone --depth=1 https://gitlab.com/CuriousNom/chaste_vendor-xiaomi-firmware-violet.git -b 14.0 vendor/xiaomi-firmware/violet
 git clone --depth=1 https://github.com/selfmusing/meme_cam.git -b 14 vendor/MiuiCamera
 git clone https://github.com/DerpFest-Devices/hardware_xiaomi.git -b 14 hardware/xiaomi
-
+wget -qO- https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main/clang-r522817.tar.gz | tar -xzf - -C prebuilts/clang/host/linux-x86/clang-r522817 > /dev/null 2>&1
 export USE_CCACHE=1 && ccache -M 60G && export CONFIG_STATE_NOTIFIER=y && export SELINUX_IGNORE_NEVERALLOWS=true
